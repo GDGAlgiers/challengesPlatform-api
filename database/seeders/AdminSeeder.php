b43b127d20b44d3fe4ba2a5c0e0fe609e9f1ac3f
Brivan-26 <ma.rezazi@esi-sba.dz>
2022-12-17 22:17:00 +0100

    Updated AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -19,21 +19,25 @@ class AdminSeeder extends Seeder
             User::create([
                 'full_name' => 'abdessamed',
                 'password' => Hash::make('5RO8TTzmUPg4'),
+                'is_member' => true,
                 'role' => 'admin'
             ]);
             User::create([
                 'full_name' => 'ouael',
                 'password' => Hash::make('FVFHAOEr3FA8'),
+                'is_member' => true,
                 'role' => 'admin'
             ]);
             User::create([
                 'full_name' => 'abderraouf',
                 'password' => Hash::make('Ui3tCLLtlpl2'),
+                'is_member' => true,
                 'role' => 'admin'
             ]);
             User::create([
                 'full_name' => 'abderrahman',
                 'password' => Hash::make('PrCiXwB5X9wy'),
+                'is_member' => true,
                 'role' => 'admin'
             ]);
     }
